9225c7ff2441fbcfe81423c551469a231baad696
Andrzej Drzystek <andrzej.drzystek@gmail.com>
2023-04-14 00:22:00 +0200

    [Step 2] Scaffold a generic game engine

--- library/src/tic_tac_toe/logic/validators.py
+++ library/src/tic_tac_toe/logic/validators.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING
 from tic_tac_toe.logic.exceptions import InvalidGameState
 
 if TYPE_CHECKING:
+    from tic_tac_toe.game.players import Player
     from tic_tac_toe.logic.models import GameState, Grid, Mark
 
 
@@ -49,3 +50,8 @@ def validate_winner(grid: Grid, starting_mark: Mark, winner: Mark | None) -> Non
         else:
             if grid.o_count != grid.x_count:
                 raise InvalidGameState("Wrong number of Os")
+
+
+def validate_players(player1: Player, player2: Player) -> None:
+    if player1.mark is player2.mark:
+        raise ValueError("Players must use different marks")
